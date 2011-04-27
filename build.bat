e7f714c77073cf59fcce4413dacbece65d3ef103
Chris Patterson <chris@phatboyg.com>
2011-04-27 10:37:14 -0500

    Updated NuGet, NuSpec

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
 @echo off
 
+@echo Environment details
+ruby --version
+call rake --version
+call gem --version
+call gem list albacore
+
 echo Building for .NET 3.5
 call rake BUILD_CONFIG_KEY=NET35
 IF NOT %ERRORLEVEL% == 0 goto FAILED
