f4221777b271bab5d2a5e4282dcb1fa57b0e3f16
Henrik <henrik@haf.se>
2012-05-28 13:34:07 +0200

    Adding nuget package restore to solution. Sln file adding the nuget folder, and changes in all csproj files to match the change in how packages are restored. Moving log4net to its own assembly but with same namespace. Now you can add that as a nuget instead and have logging optionally included rather than having it forced upon you - not all test assmeblies using TestFramework want log4net. Moving Routing to an assembly of its own also, because it depends on System.Web.

---
+++ src/Magnum.TestFramework.Routing/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Magnum.TestFramework.Routing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Magnum.TestFramework.Routing")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("221aff77-87c5-4544-aaa3-cad3bc22f283")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
